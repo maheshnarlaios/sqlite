558280ad6c066595c26ee989369e54ac7987be4b
LOGICSHORE <logicshore@LOGIC.local>
2018-04-06 16:33:41 +0530

    chicha changes

---
+++ SQLite/Chandan.h
@@ -0,0 +1,13 @@
+//
+//  Chandan.h
+//  SQLite
+//
+//  Created by LOGICSHORE on 06/04/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Chandan : UIViewController
+
+@end
---
+++ SQLite/Chandan.m
@@ -0,0 +1,39 @@
+//
+//  Chandan.m
+//  SQLite
+//
+//  Created by LOGICSHORE on 06/04/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import "Chandan.h"
+
+@interface Chandan ()
+
+@end
+
+@implementation Chandan
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSLog(@"Hoewle Mahesh ga");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
