3c8b392e61ba4c82951c37ca87cc93010d1dc03e
apple <>
2018-04-06 16:32:40 +0530

    new class

---
+++ SQLite/Mahesh.h
@@ -0,0 +1,13 @@
+//
+//  Mahesh.h
+//  SQLite
+//
+//  Created by apple on 06/04/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Mahesh : UIViewController
+
+@end
---
+++ SQLite/Mahesh.m
@@ -0,0 +1,39 @@
+//
+//  Mahesh.m
+//  SQLite
+//
+//  Created by apple on 06/04/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#import "Mahesh.h"
+
+@interface Mahesh ()
+
+@end
+
+@implementation Mahesh
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSLog(@"Mahesh File");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
